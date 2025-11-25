5fe5d4f6e3046af63f18ee06aedff71cfcabfc26
nada khairaldin <nadakhairaldin8@gmail.com>
2025-11-25 15:46:42 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/*
+features --> app features as every feature is in a separate folder to achieve decoupling 
+services --> api fetching 
+ui --> ui reuseable components
+utils --> js functions for methods like date formatting
+etc ... like pages --> but here we will separate pages inside features */ 
+function App() {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default App
